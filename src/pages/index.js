2e37faecbb6e2567556d0132e38958513173284e
Stoupe <h.stoupe@gmail.com>
2020-03-24 00:49:43 +1300

    Edit styles and add uni links

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,37 @@ const IndexPage = () => (
     <SEO title="Home" />
 
     <div class={`container`}>
-      <Bookmark title="CALENDAR" link="https://calendar.google.com/calendar/r"/>
-      <Bookmark title="ENGR 301" link="https://ecs.wgtn.ac.nz/Courses/ENGR301_2020T1/"/>
-      <Bookmark title="SWEN 301" link="https://ecs.wgtn.ac.nz/Courses/SWEN301_2020T1/"/>
-      <Bookmark title="SWEN 304" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/"/>
-      <Bookmark title="COMP 307" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/"/>
+
+    <Bookmark title="GOOGLE CALENDAR" link="https://calendar.google.com/calendar/r"/>
+
+      <h3>ENGR 301</h3>
+      <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/ENGR301_2020T1/"/>
+      <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/ENGR301_2020T1/LectureSchedule"/>
+      <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/ENGR301_2020T1/Assignments"/>
+      <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=ENGR301_2020T1"/>
+      <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/ENGR301"/>
+
+      <h3>SWEN 301</h3>
+      <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN301_2020T1/"/>
+      <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/SWEN301_2020T1/LectureSchedule"/>
+      <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/SWEN301_2020T1/Assignments"/>
+      <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN301_2020T1"/>
+      <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN301"/>
+      
+      <h3>SWEN 304</h3>
+      <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/"/>
+      <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/LectureSchedule"/>
+      <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/Assignments"/>
+      <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN304_2020T1"/>
+      <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN304"/>
+
+      <h3>COMP 307</h3>
+      <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/"/>
+      <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/LectureSchedule"/>
+      <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/Assignments"/>
+      <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=COMP307_2020T1"/>
+      <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/COMP307"/>
+    
     </div>
 
     <Link to="/page-2">PAGE 2</Link>
