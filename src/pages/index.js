c00a8958fc631e5c546f09636615e236fffaed5d
Stoupe <H.stoupe@gmail.com>
2020-07-13 01:15:50 +1200

    Update course links & fix styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,34 +36,34 @@ const IndexPage = () => (
         <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/engr301"/>
       </section>
 
-      <h3>SWEN 302</h3>
+      <h3>SWEN 324</h3>
       <section>
-        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN301_2020T1/"/>
-        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/ENGR302_2020T2/LectureSchedule"/>
-        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/ENGR302_2020T2/Assessments"/>
-        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN302_2020T2"/>
-        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN302"/>
-        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/swen302"/>
+        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T2/"/>
+        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/SWEN324_2020T2/LectureSchedule"/>
+        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/SWEN324_2020T2/Assignments"/>
+        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN324_2020T2"/>
+        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN324"/>
+        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/swen324"/>
       </section>
       
-      <h3>SWEN 304</h3>
+      <h3>SWEN 325</h3>
       <section>
-        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/"/>
-        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/LectureSchedule"/>
-        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/SWEN304_2020T1/Assignments"/>
-        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN304_2020T1"/>
-        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN304"/>
-        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/swen304"/>
+        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/SWEN325_2020T2/"/>
+        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/SWEN325_2020T2/LectureSchedule"/>
+        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/SWEN325_2020T2/Assignments"/>
+        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=SWEN325_2020T2"/>
+        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/SWEN325"/>
+        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/swen325"/>
       </section>
 
-      <h3>COMP 307</h3>
+      <h3>COMP 309</h3>
       <section>
-        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/"/>
-        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/LectureSchedule"/>
-        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/COMP307_2020T1/Assignments"/>
-        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=COMP307_2020T1"/>
-        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/COMP307"/>
-        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/comp307"/>
+        <Bookmark title="Wiki" link="https://ecs.wgtn.ac.nz/Courses/COMP309_2020T2/"/>
+        <Bookmark title="Lecture Schedule" link="https://ecs.wgtn.ac.nz/Courses/COMP309_2020T2/LectureSchedule"/>
+        <Bookmark title="Assignments" link="https://ecs.wgtn.ac.nz/Courses/COMP309_2020T2/Assignments"/>
+        <Bookmark title="Forum" link="https://ecs.wgtn.ac.nz/cgi-bin/yabb/YaBB.pl?board=COMP309_2020T2"/>
+        <Bookmark title="Submission" link="https://apps.ecs.vuw.ac.nz/submit/COMP309"/>
+        <Bookmark title="Zoom" image="zoom.png" link="https://vuw.zoom.us/my/comp309"/>
       </section>
     </div>
 
